ee72e7321b2e627f6b234ac56c1743d9c24a50e4
Jolanta Recka <jolantua@gmail.com>
2018-08-04 09:54:07 +0200

    application-form-base

---
+++ js/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RemindMe from './RemindMe/RemindMe.jsx'
+
+export default class App extends React.Component {
+ 
+    
+    render() {
+        
+        return (
+            <ApplicationForm />
+        )
+  }
+}
