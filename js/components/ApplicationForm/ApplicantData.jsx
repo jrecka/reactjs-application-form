9c548f0382a09d5173daaf683b87058583f5d4b0
Jolanta Recka <jolantua@gmail.com>
2018-08-04 16:49:22 +0200

    contact component

--- js/components/ApplicationForm/ApplicantData.jsx
+++ js/components/ApplicationForm/ApplicantData.jsx
@@ -12,92 +12,111 @@ export default class ApplicantData extends React.Component{
             dateOfBirth: '',
             birthpalce: '',
             nationality: '',
-            sex: ['female', 'male'],
+            sex: '',
             fathersName: '',
             mothersName: '',
             maidenName: '',
             street: '',
             houseNumber: '',
             flat: '',
-            postCode: '',
+            postalCode: '',
             city: '',
             email: ''
-            
+
         }
     }
-   
-    
-    
-//    handleChange = e => {
-//        this.setState({
-//           [e.target.name]: e.target.value
-//        })
-//    }
-    
-    
+
+   handleChange = e => {
+       this.setState({
+          [e.target.name]: e.target.value
+       })
+   }
+
+    handleClick = () => {
+
+    const personalData = {
+        pesel: this.state.pesel,
+        name: this.state.name,
+        lastName: this.state.lastName,
+        familyName: this.state.familyName,
+        dateOfBirth: this.state.dateOfBirth,
+        birthpalce: this.state.birthpalce,
+        nationality: this.state.nationality,
+        sex: this.state.sex,
+        fathersName: this.state.fathersName,
+        mothersName: this.state.mothersName,
+        maidenName: this.state.maidenName,
+        street: this.state.street,
+        houseNumber: this.state.houseNumber,
+        flat: this.state.flat,
+        postalCode: this.state.postalCode,
+        city: this.state.city,
+        email: this.state.email
+    }
+        console.log(personalData );
+    }
     render(){
         return(
         <div>
-          <form className='col-md-12'> 
+          <form className='col-md-12'>
           <div>Personal data</div>
           <div className="form-group">
               <label>ID:</label>
-              <input name="pesel" className='form-control' type='text' placeholder="Social Security numbers"></input>
+              <input name="pesel" className='form-control' type='text' onChange={this.handleChange} placeholder="Social Security numbers"></input>
           </div>
           <div className="form-group">
               <label>Name:</label>
-              <input name="name" className='form-control' type='text' placeholder="Name"></input>
+              <input name="name" className='form-control' type='text' onChange={this.handleChange} placeholder="Name"></input>
           </div>
           <div className="form-group">
               <label>Last name:</label>
-              <input name="lastName" className='form-control' type='text' placeholder="Last name"></input>
+              <input name="lastName" className='form-control' type='text' onChange={this.handleChange} placeholder="Last name"></input>
           </div>
           <div className="form-group">
               <label>Family name:</label>
-              <input name="familyName" className='form-control' type='text' placeholder="Family name"></input>
-          </div>          
+              <input name="familyName" className='form-control' type='text' onChange={this.handleChange} placeholder="Family name"></input>
+          </div>
            <div className="form-group">
               <label>Date of birth:</label>
-              <input name="dateOfBirth" className='form-control' type='data' placeholder="Family name"></input>
-          </div>         
+              <input name="dateOfBirth" className='form-control' type='date' onChange={this.handleChange}></input>
+          </div>
            <div className="form-group">
               <label>Birthpalce:</label>
-              <input name="birthpalce" className='form-control' type='text' placeholder="Birthplace"></input>
-          </div>          
+              <input name="birthpalce" className='form-control' type='text' onChange={this.handleChange} placeholder="Birthplace"></input>
+          </div>
             <div className="form-group">
               <label>Nationality:</label>
-              <input name="nationality" className='form-control' type='text' placeholder="Family name"></input>
-          </div>  
+              <input name="nationality" className='form-control' type='text' onChange={this.handleChange} placeholder="Family name"></input>
+          </div>
           <div className="form-check">
               <div> Sex:</div>
-              <input name="name" className="form-check-input" type="checkbox" value="female" />
+              <input name="sex" className="form-check-input" type="checkbox" onChange={this.handleChange} value="female" />
               <label className="form-check-label">
             Female
               </label>
          </div>
          <div className="form-check">
-             <input name="name" className="form-check-input" type="checkbox" value="male"/>
+             <input name="sex" className="form-check-input" type="checkbox" onChange={this.handleChange} value="male"/>
              <label className="form-check-label">Male</label>
          </div>
-         <div>Parents data</div>   
+         <div>Parents data</div>
          <div className="form-group">
              <label>Father's name:</label>
-             <input name="fathersName" className='form-control' type="text" placeholder="father's name"></input>
-         </div>    
+             <input name="fathersName" className='form-control' type="text" onChange={this.handleChange} placeholder="father's name"></input>
+         </div>
          <div className="form-group">
              <label>Mother's name:</label>
-             <input name="mothersName" className='form-control' type="text" placeholder="mother's name"></input>
-         </div> 
+             <input name="mothersName" className='form-control' type="text" onChange={this.handleChange} placeholder="mother's name"></input>
+         </div>
         <div className="form-group">
              <label>Mother's maiden name:</label>
-             <input name="maidenName" className='form-control' type="text" placeholder="mother's maiden name"/>
-         </div> 
-       
-         
+             <input name="maidenName" className='form-control' type="text" onChange={this.handleChange} placeholder="mother's maiden name"/>
+         </div>
+
          </form>
-        
-           
-            
+        <button onClick={this.handleClick}></button>
+
+
         </div>)
     }
 }
---
+++ js/components/ApplicationForm/ContactData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ContactData extends Rect.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+}
