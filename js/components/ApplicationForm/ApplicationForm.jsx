ee9d1b7ada5fbfd013b6330e9bb2eb24fd966bfa
Jolanta Recka <jolantua@gmail.com>
2018-08-05 14:27:16 +0200

    application reason added

--- js/components/ApplicationForm/ApplicationForm.jsx
+++ js/components/ApplicationForm/ApplicationForm.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApplicantData from './ApplicantData.jsx'
-import Contact from './Contact.jsx'
-import AddPhoto from './AddPhoto.jsx'
+import ApplicantData from './ApplicantData.jsx';
+import Contact from './Contact.jsx';
+import AddPhoto from './AddPhoto.jsx';
+import Reason from './Reason.jsx';
 
 export default class ApplicationForm extends React.Component{
     render(){
         return(
-            <AddPhoto/>
+            <Reason options={['-', 'First Id', 'Change of data contained in the Id', 'Id is out of date', 'Id is lost', 'Face image changed', 'Damage of Id', 'Another reason']}/>
         )
     }
 }
---
+++ js/components/ApplicationForm/Reason.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Reason extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            reasonOfApplication: '',
+            message: ''
+        }
+    }
+
+    showTextarea = () => {
+
+        let textarea = document.querySelector('textarea'),
+            selectVal = document.querySelector('select').value;
+        if( selectVal === "Another reason") {
+            textarea.classList.remove('show-or-hide');
+        }else{
+            textarea.classList.add('show-or-hide');
+        }
+    }
+
+
+
+    handleChange = e => {
+
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+
+        this.showTextarea();
+
+    }
+
+
+    handleClick = () => {
+    console.log(this.state.reasonOfApplication);
+    console.log(this.state.message);
+    }
+
+    render(){
+        return(
+            <div className="row">
+                <form className="col-md-12">
+                    <div className="form-group">
+
+
+
+                        <label>
+                            Reason for submitting the application
+                        </label>
+                        <select name="reasonOfApplication"
+                                className="form-control"
+                                value={this.state.reasonOfApplication}
+                                onChange={this.handleChange}>
+                            {this.props.options.map( (option,i) => <option key={i} value={option}>{option}</option>)}
+                        </select>
+                        <textarea name="message" value={this.state.message} onChange={this.handleChange} className="show-or-hide" rows="4" cols="50"></textarea>
+
+                    </div>
+                </form>
+<button onClick={this.handleClick}>clickclickclick</button>
+            </div>
+        )
+    }
+
+}
