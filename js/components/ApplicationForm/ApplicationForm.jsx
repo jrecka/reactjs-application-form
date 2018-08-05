e33767a99b8aa8bcf7b3cc129148abf0c8346659
Jolanta Recka <jolantua@gmail.com>
2018-08-05 15:55:16 +0200

    signature component added

--- js/components/ApplicationForm/ApplicationForm.jsx
+++ js/components/ApplicationForm/ApplicationForm.jsx
@@ -4,11 +4,15 @@ import ApplicantData from './ApplicantData.jsx';
 import Contact from './Contact.jsx';
 import AddPhoto from './AddPhoto.jsx';
 import Reason from './Reason.jsx';
+import Signature from './Signature/Signature.jsx';
 
 export default class ApplicationForm extends React.Component{
     render(){
         return(
-            <Reason options={['-', 'First Id', 'Change of data contained in the Id', 'Id is out of date', 'Id is lost', 'Face image changed', 'Damage of Id', 'Another reason']}/>
+            <div>
+                <Signature options={['1', '2', '3', '4', '5', '6']}/>
+                <Reason options={['-', 'First Id', 'Change of data contained in the Id', 'Id is out of date', 'Id is lost', 'Face image changed', 'Damage of Id', 'Another reason']}/>
+            </div>
         )
     }
 }
---
+++ js/components/ApplicationForm/Declatarion.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Declatarion extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: '',
+            date: '',
+        }
+    }
+    }
